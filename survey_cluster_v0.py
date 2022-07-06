29efde404f7f81c35b08bef18e8ceae4c7a85713
Calvin Aberg <98669438+calvinaberg1@users.noreply.github.com>
2022-07-06 16:51:17 -0400

    Upload initial survey clustering script

---
+++ survey_cluster_v0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 14:10:42 2022
+
+@author: caberg
+"""
+# read ins
+import pandas as pd
+inodir = 'C:/Users/caberg/OneDrive - MetLife/Desktop/innovation challenge'
+survey = pd.read_excel(inodir + '/MEET-MET Personality Matching Quiz(1-1).xlsx')
+
